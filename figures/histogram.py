65e234a54ec16aa46f49fcee0bd07b0b850c6dfe
AntonRydahl <rydahl2610@gmail.com>
2023-07-05 11:45:10 -0700

    Changed the vertical axis to counts for timing figures

--- figures/histogram.py
+++ figures/histogram.py
@@ -20,9 +20,9 @@ med_p2 = np.median(p2)
 
 bins = np.linspace(min(min(p1),min(p2)), max(max(p1),max(p2)), 100)
 
-plt.hist(p1, bins, color='cornflowerblue', alpha=0.5, density=True, label=f" {builtin}")
+plt.hist(p1, bins, color='cornflowerblue', alpha=0.5, label=f" {builtin}")
 plt.axvline(x = med_p1, color='cornflowerblue', label=f" median({builtin})={np.round(med_p1,2)}")
-plt.hist(p2, bins, color='orange', alpha=0.5, density=True, label=f" {vendor}")
+plt.hist(p2, bins, color='orange', alpha=0.5, label=f" {vendor}")
 plt.axvline(x = med_p2, color='orange', label=f" median({vendor})={np.round(med_p2,2)}")
 plt.legend(loc='upper right')
 plt.xlabel('Wall Time in MS',fontsize=15)
